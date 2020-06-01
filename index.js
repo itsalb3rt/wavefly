f2f26f0cee24b7c54c320a62ba604cfa71ba6f65
Albert Hidalgo <ahidalgo@syneteksolutions.com>
2020-06-01 16:03:50 -0400

    removed unnecesarry function

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 var SoxCommand = require('sox-audio');
 var through = require('through');
-/* Streaming examples */
 
+/**
+ * Debug stuff
+ * @param {*} command 
+ */
 var addStandardListeners = function (command) {
   command.on('prepare', function (args) {
     console.log('Preparing sox command with args ' + args.join(' '));
@@ -27,38 +30,16 @@ var addStandardListeners = function (command) {
   });
 };
 
-var genThroughStreamCounter = function () {
-  var dataCounter = 0;
-  var throughStream = through(
-    function write(data) {
-      dataCounter += data.length;
-      console.log("ThroughStreamCounter: Writing through data ", data.length);
-      this.queue(data);
-    },
-    function end() {
-      console.log("ThroughStreamCounter: END. Wrote through data ", dataCounter);
-      this.queue(null);
-    });
-  return throughStream;
-};
-
-/* Transcodes a wav input stream from 41k to 16k, streaming out a 16k wav file
-to outputPipe. Optionally pass useThrough = true to insert a through stream
-between the wavInputStream and the SoxCommand to count and print the bytes of data */
-var transcodeWavStream = function (wavInputStream, outputSampleRate, outputPipe, useThrough) {
-  useThrough = useThrough || false;
+/* 
+Transcodes a wav input stream
+*/
+var transcodeWavStream = function (wavInputStream, outputSampleRate, channels, outputPipe) {
   var inputStream = wavInputStream;
-
-  if (useThrough) {
-    var throughStreamCounter = genThroughStreamCounter();
-    wavInputStream.pipe(throughStreamCounter);
-    inputStream = throughStreamCounter;
-  }
 
   var command = SoxCommand();
   command.input(inputStream)
     .inputEncoding('signed')
-    .inputChannels(1)
+    .inputChannels(channels)
     .inputFileType('wav')
     .output(outputPipe)
     .outputFileType('wav')
@@ -70,6 +51,6 @@ var transcodeWavStream = function (wavInputStream, outputSampleRate, outputPipe,
 
 // Load a wav file buffer as a WaveFile object
 var wavInputStream = fs.createReadStream('example_audio/source4.wav');
-var outputFileStream = fs.createWriteStream('example_audio/out.wav');
+var outputFileStream = fs.createWriteStream('example_audio/out_audio/out.wav');
 
-transcodeWavStream(wavInputStream, 8000, outputFileStream);
+transcodeWavStream(wavInputStream, 8000, 1, outputFileStream);

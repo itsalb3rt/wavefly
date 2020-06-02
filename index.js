c4072b95466e6834537664a2f18f338450d5cd18
Albert Hidalgo <ahidalgo@syneteksolutions.com>
2020-06-02 09:20:58 -0400

    feat
    now parse any format file to wav

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 var SoxCommand = require('sox-audio');
-var through = require('through');
+const musicMetadata = require('music-metadata');
 
 /**
  * Debug stuff
@@ -31,16 +31,16 @@ var addStandardListeners = function (command) {
 };
 
 /* 
-Transcodes a wav input stream
+* Transcodes a wav input stream
 */
-var transcodeWavStream = function (wavInputStream, outputSampleRate, channels, outputPipe) {
+var transcodeWavStream = function (wavInputStream, inputFileType, outputSampleRate, channels, outputPipe) {
   var inputStream = wavInputStream;
 
   var command = SoxCommand();
   command.input(inputStream)
     .inputEncoding('signed')
     .inputChannels(channels)
-    .inputFileType('wav')
+    .inputFileType(inputFileType)
     .output(outputPipe)
     .outputFileType('wav')
     .outputSampleRate(outputSampleRate);
@@ -49,8 +49,25 @@ var transcodeWavStream = function (wavInputStream, outputSampleRate, channels, o
   command.run();
 };
 
-// Load a wav file buffer as a WaveFile object
-var wavInputStream = fs.createReadStream('example_audio/source4.wav');
-var outputFileStream = fs.createWriteStream('example_audio/out_audio/out.wav');
+const getFormat = (container) => {
+  const formats = [
+    {type:'WAVE',value:'wav'},
+    { type:'MPEG',value:'mp3'},
+  ]
+  let result = formats.find(format => format.type === container);
+  return result.value;
+}
 
-transcodeWavStream(wavInputStream, 8000, 1, outputFileStream);
+// Load a wav file buffer as a WaveFile object
+const inputFilePath = 'example_audio/source2.mp3';
+let wavInputStream = fs.createReadStream(inputFilePath);
+let outputFileStream = fs.createWriteStream('example_audio/out_audio/out.wav');
+
+musicMetadata.parseFile(inputFilePath)
+  .then(metadata => {
+    let inputFileType = getFormat(metadata.format.container);
+    transcodeWavStream(wavInputStream, inputFileType, 8000, 1, outputFileStream);
+  })
+  .catch(err => {
+    console.error(err.message);
+  });

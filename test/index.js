c74a045ac322a26157203f860849ff6cfa7f0ad2
Albert Hidalgo <ahidalgo@syneteksolutions.com>
2020-06-02 11:25:19 -0400

    test
    added test

---
+++ test/index.js
@@ -0,0 +1,7 @@
+const wavefly = require('../src/index');
+
+// test
+const inputFilePath = './src/example_audio/source6.flac';
+const outputFilePath = './src/example_audio/out_audio/out.wav';
+
+wavefly(inputFilePath, outputFilePath);

8f81a69ce1194aa791be28976dfd33e0ed0de2b6
Albert Hidalgo <ahidalgo@syneteksolutions.com>
2020-06-02 11:45:05 -0400

    refactor
    moved example audio to test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 const wavefly = require('../src/index');
 
 // test
-const inputFilePath = './src/example_audio/source6.flac';
-const outputFilePath = './src/example_audio/out_audio/out.wav';
+const inputFilePath = './test/example_audio/source6.flac';
+const outputFilePath = './test/example_audio/out_audio/out.wav';
 
 wavefly(inputFilePath, outputFilePath);
